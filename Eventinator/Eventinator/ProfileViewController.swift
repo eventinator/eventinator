51a02994778a25711d211695a41f807910339184
Jon O'Keefe <jonokeefe@gmail.com>
2016-11-09 00:11:21 -0800

    Initial tab bar layout with view controllers
    https://github.com/eventinator/eventinator/issues/6

---
+++ Eventinator/Eventinator/ProfileViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileViewController.swift
+//  Eventinator
+//
+//  Created by Jon O'Keefe on 11/9/16.
+//  Copyright © 2016 Eventinator. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let logo = UIImage(named: "lineup-logo.png")
+        let imageView = UIImageView(image: logo)
+        self.navigationItem.titleView = imageView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
