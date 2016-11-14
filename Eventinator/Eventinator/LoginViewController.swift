25fe9277b88b69ce52bae1a351a98e57d1cb13ca
miketehranian <mike.tehranian@gmail.com>
2016-11-14 13:53:57 -0800

    Added signup code with Parse

--- Eventinator/Eventinator/LoginViewController.swift
+++ Eventinator/Eventinator/LoginViewController.swift
@@ -7,37 +7,68 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
     @IBAction func onLoginClick(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
-        self.present(mainViewController, animated: true, completion: nil)
+        // Verify that the user is valid
+        let user = PFUser()
+        user.username = usernameTextField.text!
+        user.password = passwordTextField.text!
+        
+        PFUser.logInWithUsername(inBackground: usernameTextField.text!, password: passwordTextField.text!)
+        { (pfUser:PFUser?, error:Error?) in
+            if let error = error {
+                dump(error)
+                let alert = UIAlertAction(title: "OK", style: .default, handler: nil)
+                let alertController = UIAlertController(title: "Log in Error", message: error.localizedDescription, preferredStyle: .alert)
+                alertController.addAction(alert)
+                self.present(alertController, animated: true, completion: nil)
+            } else {
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+                self.present(mainViewController, animated: true, completion: nil)
+            }
+        }
     }
-
+    
+    @IBAction func onSignupTap(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameTextField.text!
+        user.password = passwordTextField.text!
+        
+        user.signUpInBackground { (succeeded: Bool, error: Error?) in
+            if let error = error {
+                let alert = UIAlertAction(title: "OK", style: .default, handler: nil)
+                let alertController = UIAlertController(title: "Sign up Error", message: error.localizedDescription, preferredStyle: .alert)
+                alertController.addAction(alert)
+                self.present(alertController, animated: true, completion: nil)
+            } else {
+                let alert = UIAlertAction(title: "OK", style: .default, handler: nil)
+                let alertController = UIAlertController(title: "Sign Up Successful", message: "You signed up for an account successfully!", preferredStyle: .alert)
+                alertController.addAction(alert)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
